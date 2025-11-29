1bf8fffcd043c7902fb647376becb85cface0ea7
Itsrendering <159409670+Itsrendering@users.noreply.github.com>
2025-11-29 10:53:18 -0500

    Update queries.sql
    Add default rate analysis

--- queries.sql
+++ queries.sql
@@ -3,3 +3,10 @@
 SELECT 
     SUM(loan_amount) AS total_loan_amount
 FROM loan_data;
+
+
+-- 2. Overall Loan Default Rate
+SELECT 
+    COUNT(CASE WHEN loan_status IN ('Charged Off', 'Default') THEN 1 END) * 1.0 / COUNT(*) 
+    AS default_rate
+FROM loan_data;
